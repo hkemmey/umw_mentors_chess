87b30c4be68fdbe56fb74742bfa54c7d04cda7c5
Kevin Orellana <kaorellana52@gmail.com>
2022-02-01 14:14:10 -0500

    Largely finished parent piece class & a little bit of board was worked on
    We wrote methods for the piece class that check and return which team a given piece is on, and methods that change said attributes.

--- src/Board.py
+++ src/Board.py
@@ -1,13 +1,20 @@
 class Board:
-    def __init__(self):
-        board = [[0, 1, 2, 3, 4, 5, 6, 7][0, 1, 2, 3, 4, 5, 6, 7]]
 
-    def getPiece(self);
+    board = [[], [], [], [], [], [], [], []] #fill later
+    def __init__(self):
+        pass
+
+
+    def resetBoard(self):
+        pass
+    def getPiece(self):
         #get a piece from a specified coordinate
         #also check if the specified spot is occupied by a piece
+        pass
 
-    def printBoard(self)
+    def printBoard(self):
         #print all the pieces that are on the board as 2 letter symbols
         #example: bK = black King, wKn = white knight
         #empty spaces represented by "[_]"
         #maybe divide out all spaces with | and _
+        pass
--- src/Piece.py
+++ src/Piece.py
@@ -1,12 +1,24 @@
 from abc import ABC, abstractmethod
 
 class Piece(ABC):
-    def __init__(self, team):
-        #defines all the traits that all pieces have in common
-        #coordinates and team are shared traits
-    def getCoordinate(self):
-        #retrieves the coordinate of the piece as 2 integers, xCoord and YCoorc
-    def getTeam(self):
-        #retrieves the team of the piece as a boolean
+    dead = False
+    team = False #VERY IMPORTANT: False = Black, True = White
+    
+    def __init__(self, team): #white is just a boolean that decides team
+        self.t = team
 
-        
+    def updateTeam(self, team):
+        self.t = team
+
+    def teamCheck(self):
+        return self.t
+
+    def lifeUpdate(self, dead):
+        self.life = dead
+
+    def lifeStatus(self):
+        return self.life
+    
+
+def pawn(Piece):
+    pass
