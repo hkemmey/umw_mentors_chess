7260df6c04cdae6f806c055301b42d82bd0e3faf
Kevin Orellana <kaorellana52@gmail.com>
2022-02-07 14:15:15 -0500

    Created the setImage method in each piece subclass & defined a checkLegalMoves
    checkLegalMoves was left with nothing in it for now, and we are trying to find a better way to utilize the setImage class, since the current process feels very cluttered and repetitive to the point that it warrants a spot in the parent class.  We just need to figure out how to do that.

--- src/Piece.py
+++ src/Piece.py
@@ -1,5 +1,5 @@
 #create each individual piece's subclass so that they know what image they use
-#also create outline, whilst using the parent class effectively
+#also create outline of each class, whilst using the parent class effectively
 
 from abc import ABC, abstractmethod
 
@@ -22,6 +22,92 @@ class Piece(ABC):
     def lifeStatus(self):
         return self.life
     
+    def getType(self):
+        return self.t #CONTINUE FROM HERE: consider moving setImage here for more organized code, if
+        #possible
+    
 
 def pawn(Piece):
+    def __init__(self):
+        Piece.__init__(team)
+        self.t = "pawn"
+
+    def setImage(self, team):
+        if (team == False):
+            image = "bPawn.png"
+        else:
+            image = "wPawn.png"
+    
+    def checkLegalMoves(self): #would this have to be defined in every class, or just the...
+        #...parent class?
+        pass
+    #make a class that checks the position of the pawn, and sees if it has reached the other side
+
+def rook(Piece):
+    def __init__(self):
+        Piece.__init__(team)
+
+    def setImage(self, team):
+        if (team == False):
+            image = "bRook.png"
+        else:
+            image = "wRook.png"
+    
+    def checkLegalMoves(self):
+        pass
+
+def knight(Piece):
+    def __init__(self):
+        Piece.__init__(team)
+
+    def setImage(self, team):
+        if (team == False):
+            image = "bKnight.png"
+        else:
+            image = "wKnight.png"
+    
+    def checkLegalMoves(self):
+        pass    
+    pass
+
+def bishop(Piece):
+    def __init__(self):
+        Piece.__init__(team)
+
+    def setImage(self, team):
+        if (team == False):
+            image = "bBishop.png"
+        else:
+            image = "wBishop.png"
+    
+    def checkLegalMoves(self):
+        pass    
+    pass
+
+def queen(Piece):
+    def __init__(self):
+        Piece.__init__(team)
+
+    def setImage(self, team):
+        if (team == False):
+            image = "bQueen.png"
+        else:
+            image = "wQueen.png"
+    
+    def checkLegalMoves(self):
+        pass
+    pass
+
+def king(Piece):
+    def __init__(self):
+        Piece.__init__(team)
+
+    def setImage(self, team):
+        if (team == False):
+            image = "bKing.png"
+        else:
+            image = "wKing.png"
+    
+    def checkLegalMoves(self):
+        pass    
     pass
