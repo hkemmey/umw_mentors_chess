c0b0af418eaeb23e85a1a83bc1d88218cf1e1292
hkemmey <harrison.kemmey@gmail.com>
2022-02-09 20:36:25 -0500

    draw a chess board and test it in the main

--- src/chess.py
+++ src/chess.py
@@ -1,16 +1,74 @@
-import pygame as pg
+import pygame
 
-WIDTH = HEIGHT = 512
-SPOTS = 8
-SQ_SIZE = HEIGHT/SPOTS
+WIDTH = HEIGHT = 1024
+NUM_SQUARES_PER_ROW = 8
+SQ_SIZE = HEIGHT/NUM_SQUARES_PER_ROW
+COLOR_LIGHT = "#FEE3BF"
+COLOR_DARK = "#B28E5F"
+BLACK_LINE = "#000000"
 
+# initialize a pygame screen
+# draw a grid
+# ligther color and a darker color alternating squares
+# Vertically, A-H, 1-8 Horizontally
+# 
+# Put the images onto the board
+# return screen
+def draw_board(screen):
 
-def main():
-    pg.init()
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
-    while 1:
-        screen.fill((255, 255, 255))
-        pg.display.flip()
+    x = 0 #offset to the right
+    y = 0 #offset downward
+
+    prev_is_light = False
+
+    while y <= HEIGHT - SQ_SIZE:
+        if prev_is_light:
+            #draw dark
+            ##pygame.draw.rect(display, color, (x, y, SQ_SIZE, SQ_SIZE))
+            ##pygame.draw.line(display, color, (x1, y1), (x2, y2))
+            pygame.draw.rect(chess_board, COLOR_DARK, (x, y, SQ_SIZE, SQ_SIZE))
+
+        else:
+            #draw light
+            pygame.draw.rect(chess_board, COLOR_LIGHT, (x, y, SQ_SIZE, SQ_SIZE))
+
+        prev_is_light = True if prev_is_light == False else False
+
+        if x < (WIDTH - SQ_SIZE):
+            x += SQ_SIZE
+        else:
+            y += SQ_SIZE
+            x = 0
+            prev_is_light = True if prev_is_light == False else False
+
+    x = 0
+    y = 0
+
+    while y < HEIGHT:
+        if x < WIDTH:
+            pygame.draw.line(screen, BLACK_LINE, (x, 0), (x, HEIGHT))
+            x += SQ_SIZE
+        else:
+            pygame.draw.line(screen, BLACK_LINE, (0, y), (WIDTH, y))
+            y += SQ_SIZE
+
 
 if __name__ == "__main__":
-    main()
+
+    pygame.init()
+    chess_board = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    draw_board(chess_board)
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+                
+
+
+    
