10f9fb77365d27231f47df6c6e3564598b1b7a99
Kevin Orellana <kaorellana52@gmail.com>
2022-02-17 06:22:11 -0500

    Worked on filling the board with pieces

--- src/Board.py
+++ src/Board.py
@@ -1,6 +1,7 @@
 #Forswyth Edwards Notation (look up "string notation chess")
 #do this if piece class stuff is already done with for now
 #see if we can build a board class with said notation
+import Piece
 
 class Board:
 
@@ -11,20 +12,41 @@ class Board:
 
     def __init__(self, boardString):
         #use boardString here to piece together the board here
+        row = 0
         for x in boardString:
             if (boardString[x:x+1] == " "):
                 break
 
             currentChar = boardString[x:x+1]
             #no chance to test if this will work; my first time working with try statements
+            #also another thing im trying to figure out is whether or not its okay to really use this as a sort of/
+            #if/else statement or not, mostly since the point of this is mostly just to prevent errors causing
+            #the program to stop - Kevin
             try:
-                currentChar = int(currentChar)
+                blank_length = int(currentChar)
             except:
-                pass
-            else:
-                pass
-
+                #match statement would be more functional, but the linter does not
+                #recognize it as valid
+                if (currentChar == "/"):
+                    row += 1
+                elif (currentChar == "r"): #lower case letter = black, black = False
+                    board[row].append(Piece.rook(False))
+                elif (currentChar == "R"):
+                    board[row].append(Piece.rook(True))
+                elif (currentChar == "n"):
+                    board[row].append(Piece.knight(False))
+                elif (currentChar == "N"):
+                    board[row].append(Piece.knight(True))
+                elif (currentChar == "b"):
+                    board[row].append(Piece.bishop(False))
+                elif (currentChar == "B"):
+                    board[row].append(Piece.bishop(True))
                 
+            
+            else:
+                for i in blank_length:
+                    #None is pythons version of null
+                    board[row].append(None)
 
 
     def resetBoard(self):
